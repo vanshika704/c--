8e7a83f176d66427ffd1d949668959e35268a21c
vanshika704 <sharmavanshi704@gmail.com>
2023-10-30 09:42:14 +0530

    first commit

---
+++ rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int i;
+    cin>>i;
+    for(int k=1;k<=i;k++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+   
+   
+}

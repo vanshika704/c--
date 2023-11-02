1b6ebb63b37eb68da9b755e954bee0af4c9d9e0c
vanshika704 <sharmavanshi704@gmail.com>
2023-11-02 22:56:25 +0530

    normal commit

---
+++ arrry.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int marks[100], n,i, sum=0+i;
+    float average;
+    for(i=0;i<n;i++){
+        printf("enter number");
+        scanf("%d",&marks[i]);
+    }
+    for(i=0;i<n;i++){
+        sum=sum+marks[i];
+average= sum/n;}
+printf("%f",average);
+    }
+
